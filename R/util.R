150556bcd5b10575ae264b528616fde77bc8bf46
alejandro baranek <alejandrobaranek@gmail.com>
2022-04-15 12:50:14 -0300

    Solution for windows webshot bug

--- R/util.R
+++ R/util.R
@@ -175,3 +175,29 @@ getPackageDir <- function() {
   }
   file.path(home.dir, data.subdir)
 }
+
+#' getOs
+#' @description
+#'  Returns OS identifier
+#' @examples
+#' getOs()
+#' @export
+getOs <- function() {
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)) {
+    os <- sysinf["sysname"]
+    if (os == "Darwin") {
+      os <- "osx"
+    }
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os)) {
+      os <- "osx"
+    }
+    if (grepl("linux-gnu", R.version$os)) {
+      os <- "linux"
+    }
+  }
+  tolower(os)
+}
+
